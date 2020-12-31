5bb0d289c4eb19de824b3b62bcc14c1a8cd1c86c
Nikita Pashinsky <nikitapashinsky@gmail.com>
2020-12-31 21:40:38 +0300

    Implement basic login

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,10 @@ module UsersHelper
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
+
+    def nav_gravatar_for(current_user, size: 200)
+        gravatar_id = Digest::MD5::hexdigest(current_user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: current_user.name, class: "nav-gravatar")
+    end
 end
